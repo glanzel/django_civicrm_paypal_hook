23f2319d09b52f4547a20a769f10140b1b140981
grischan <grischan@gmail.com>
2024-05-07 12:01:25 +0200

    process Hook2

--- main/urls.py
+++ main/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from . import views
 from django.contrib import admin
 from django.urls import path, include
 from .hello import urls as hello_urls
 
 #from .hello import views as hello_views
 from djpaypal import views as paypal_views
-import views
 
  
 
